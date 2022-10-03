9f47b2055b1d9f5d36850d291c8865393c71a04d
A. Balmaseda <aitor.5502.b@gmail.com>
2022-10-03 09:59:13 +0200

    Caterina bootloader addes and oled deactivated

--- keyboards/splitkb/kyria/keymaps/eryx5502/rules.mk
+++ keyboards/splitkb/kyria/keymaps/eryx5502/rules.mk
@@ -1,4 +1,5 @@
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+BOOTLOADER = caterina # To specify the mcu
+# OLED_ENABLE = yes
+# OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
